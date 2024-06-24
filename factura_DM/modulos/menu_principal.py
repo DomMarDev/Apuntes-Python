d7ef3d37b1108db6948f96c38239c009b9ccb629
DomMarDev <172400693+DomMarDev@users.noreply.github.com>
2024-06-24 18:06:10 +0200

    Proyecto Plantilla PDF

--- factura_DM/modulos/menu_principal.py
+++ factura_DM/modulos/menu_principal.py
@@ -1,6 +1,7 @@
 # Importamos las librerías de Tkinter y la de utilidades de ventana para centrarla y leerla.
 # Fuente: https://www.youtube.com/watch?v=fDyO2vKrSfw&t=6s&ab_channel=Autodidacta
 import tkinter as tk
+from tkinter import ttk, messagebox
 from tkinter.font import BOLD
 from pathlib import Path
 
@@ -32,7 +33,48 @@ class MenuPrincipal:
         label = tk.Label(self.ventana, image = logo, bg = '#fcfcfc')
         label.place(x=0, y=0, relwidth=1, relheight=1)
 
+
+
+
         self.ventana.mainloop()
+
+
+
+# Funciones del menú:
+
+class Factura:
+
+    def crear_factura(self):
+        '''Método que permite crear factura'''
+        user = self.usuario.get()
+        password = self.contrasena.get()
+
+        if (user == 'user' and password == '1234'):
+            self.ventana.destroy() # Eliminamos la ventana
+            MenuPrincipal() # Muestra el menú principal
+        else:
+            messagebox.showerror(message = "Las credenciales no son correctas", title = "Error")
+
+
+
+# Botones
+
+        # Creando el botón crear factura
+        crear= tk.Button(frame_MenuCrear,
+                                text = 'Crear Factura',
+                                font = ('Times', 15, BOLD),
+                                bg = '#3a7ff6',
+                                bd = 0,
+                                fg = '#fff',
+                                command = self.crear_factura)
+        crear.pack(fill = tk.X, padx = 20, pady = 20)
+
+        # Asignación de user y pass
+        
+        crear.bind('<Return>', (lambda event: self.crear_factura())) # Si damos click con un enter lanzamos el método crear factura
+
+
+
 
 
 
