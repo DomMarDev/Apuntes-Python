cc541b71249b5080f735b67605e2f8f12522fac9
Domingo Marchan <172400693+DomMarDev@users.noreply.github.com>
2024-07-03 19:02:34 +0200

    hasta aqui nosotros introduciendo datos en matplotlib

--- matplotlib/2dados.py
+++ matplotlib/2dados.py
@@ -12,18 +12,21 @@ class Dado:
         ''' Devuelve un número aleatorio entre 1 y el número de caras'''
         return randint(1, self.num_caras)
     
-miDado = Dado() #instanciamos
+dado_1 = Dado(8) #instanciamos
+dado_2 = Dado(12) #instanciamos
 
 misTiradas = [] # acumular datos del dado
 
 for tirada in range(1000):
-    tirada = miDado.tirada()
+    tirada = dado_1.tirada() + dado_2.tirada()
     misTiradas.append(tirada)
 
 # print(misTiradas[:100]) # que muestre las 100 primeras
 
 frecuencias = []
-analisis_resultado = range(1, miDado.num_caras+1) # son los 6 lados del dado
+resultado_max = dado_1.num_caras + dado_2.num_caras
+
+analisis_resultado = range(2, resultado_max+1)  # minimo de 2 dados es 2
 for valor in analisis_resultado:
     frecuencia = misTiradas.count(valor)
     frecuencias.append(frecuencia)
@@ -36,8 +39,13 @@ etiquetas= {
     'y': 'Frecuencia de resultados'
 }
 
-fig = px.line(x=analisis_resultado, y = frecuencias, title= titulo, labels= etiquetas)
+fig = px.bar(x=analisis_resultado, y = frecuencias, title= titulo, labels= etiquetas)
+# fig.update_layout(xaxis_dtick= 1) # que el axis aparezca en cada uno de los números
+
 fig.show() 
 # se puede guardar etc en el navegador que te genera el host local. Se genera un html
 
 # podemos poner line (gráfico de líneas) en vez de bar, scatter (gráfico de puntos)
+
+# para guardar directamente
+fig.write_html('frecuencia_tirada_dos_dados.html')
